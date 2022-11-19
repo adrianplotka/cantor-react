fd0e54332c73f34fe1b9c0ae63e22e43e436fc96
Adrian Płotka <adrianplotkaa@gmail.com>
2022-11-19 18:34:56 +0100

    correcting code

--- src/currencies/currencies.js
+++ src/currencies/currencies.js
@@ -1,7 +1,22 @@
 export const currencies = [
-    {short: "EUR", name: "Euro", rate:4.8019 },
-    {short: "USD", name: "Dolar Amerykański", rate:4.9382 },
-    {short: "CHF", name: "Frank Szwajcarski", rate:4.9190 },
-    {short: "GBP", name: "Funt Szterling", rate:5.5168 }
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.8019
+    },
+    {
+        short: "USD",
+        name: "Dolar Amerykański",
+        rate: 4.9382
+    },
+    { 
+        short: "CHF", 
+        name: "Frank Szwajcarski", 
+        rate: 4.9190 
+    },
+    { short: "GBP",
+     name: "Funt Szterling",
+      rate: 5.5168 
+    }
 ];
 
